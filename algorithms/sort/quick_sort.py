3caced4c075085349ef6789f9d5a256cf2aee854
Lucas Henrique <hns.lucass@gmail.com>
2024-06-07 19:03:42 -0300

    refactor: change list parameters to arr

--- algorithms/sort/quick_sort.py
+++ algorithms/sort/quick_sort.py
@@ -1,30 +1,30 @@
 import random
 import time
 
-def partition(list,begin,end):
-    pivot = list[end]
+def partition(arr,begin,end):
+    pivot = arr[end]
     i = begin
     for j in range(begin,end):
-        if list[j] <= pivot:
-            list[j],list[i] = list[i],list[j]
+        if arr[j] <= pivot:
+            arr[j],arr[i] = arr[i],arr[j]
             i += 1
     
-    list[i],list[end] = list[end],list[i]
+    arr[i],arr[end] = arr[end],arr[i]
     return i
 
-def quickSort(list,begin=0,end=None):
+def quickSort(arr,begin=0,end=None):
     if end == None:
-        end = len(list)-1
+        end = len(arr)-1
     
     if begin < end:
-        p = partition(list,begin,end)
-        quickSort(list,begin,p-1)
-        quickSort(list,p+1,end)
+        p = partition(arr,begin,end)
+        quickSort(arr,begin,p-1)
+        quickSort(arr,p+1,end)
 
-    return list
+    return arr
 
 
-random_arr = random.sample(range(1,3000001),3000000)
+random_arr = random.sample(range(1,3000001),000000)
 
 repeated = [1,1,1,1,7,7,7,7,7,5,2,0,3,9,11,19,36,23,7,9]
 
