c4ffa57b96f60bbb2f17406d6f5a07f97172363d
Lucas Henrique <hns.lucass@gmail.com>
2024-06-16 14:20:54 -0300

    feat: add knapsack 0/1 problem resolution by dynamic programming algorithm

--- algorithms/dynamic_programming/knapsack_problem.py
+++ algorithms/dynamic_programming/knapsack_problem.py
@@ -1,6 +1,22 @@
-def knapsack():
-    
-    pass
+def knapsack(weights,values,max_weight):
+    n = len(values)
+    dp = [[0]*(max_weight+1) for _ in range(n)]
+
+    for i in range(n):
+        for w in range(max_weight+1):
+            if weights[i-1] <= w:
+                dp[i][w] = max(dp[i-1][w],dp[i-1][w-weights[i-1]] + values[i-1])
+            else:
+                dp[i][w] = dp[i-1][w]
+
+    return dp[n-1][max_weight],dp
 
 if __name__ == "__main__":
-    pass
+    weights = [3,1,2,2,1]
+    values = [10,3,9,5,6]
+    max_weight = 6
+
+    max_value,dp = knapsack(weights,values,max_weight)
+    for line in dp:
+        print(line)
+    print(f"Max value: {max_value}")
--- algorithms/dynamic_programming/longest_common_substring.py
+++ algorithms/dynamic_programming/longest_common_substring.py
@@ -1,9 +1,9 @@
 def longest_common_substring(word_one,word_two):
-    grid = [[0] * len(word_one) for _ in range(len(word_two))]
+    grid = [[0]*len(word_one) for _ in range (len(word_two))]
     for letter_one in range(len(word_one)):
         for letter_two in range(len(word_two)):
             if word_one[letter_one] == word_two[letter_two]:
-                grid[letter_one][letter_two] = grid[letter_one-1][letter_two-1] + 1
+                grid [letter_one][letter_two] = grid[letter_one-1][letter_two-1] + 1
             else:
                 grid[letter_one][letter_two] = 0
 
@@ -12,7 +12,7 @@ def longest_common_substring(word_one,word_two):
 
 if __name__ == "__main__":
     word1 = "fish"
-    word2 = "hish"
+    word2 = "fosh"
     similarity_grid = longest_common_substring(word1,word2)
     for line in similarity_grid:
         print(line)
